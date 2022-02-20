2945f9f0503a764b79ad9fafff22411394b192fe
Andrei Lihtgolt <a.lihtgolt@gmail.com>
2022-02-20 19:18:29 +0200

    video

--- src/components/main/components-main/who-we-are/Whoweare.js
+++ src/components/main/components-main/who-we-are/Whoweare.js
@@ -14,7 +14,7 @@ const Whoweare = () => {
                 </div>
                 <div className="whoweare__body">
                     <div className="whoweare__video">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/EadeOYFAzJ4"
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/Owt-TQuvivo"
                                 title="YouTube video player" frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 allowFullScreen></iframe>
