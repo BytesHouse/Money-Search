df69f73d06af732c594b4c6dcc3481e73c5406bc
Andrei Lihtgolt <a.lihtgolt@gmail.com>
2022-02-21 20:08:54 +0200

    analitics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
 import {Helmet} from 'react-helmet';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "G-FSRWT0NCWQ"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
     return (
@@ -14,6 +18,7 @@ function App() {
                     <script src='https://oplata.qiwi.com/popup/v1.js'></script>
                 </Helmet>
             </div>
+
         </div>
 );
 }
